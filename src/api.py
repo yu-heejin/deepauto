10ad840f4f3921a7fc7cb26abdf2958bd138c1da
yu-heejin <jkgt0521@naver.com>
2025-08-05 17:26:56 +0900

    feat: workflow 실행 시 DB 저장 및 비동기 task 실행 기능 추가

--- src/api.py
+++ src/api.py
@@ -1,15 +1,13 @@
-from fastapi import APIRouter
-from src.agent import data_collector_agent, itinerary_builder_agent, budget_manager_agent, report_generator_agent
+from fastapi import APIRouter, BackgroundTasks
+
+from src.crud import start_workflow
 
 agent_api = APIRouter(
     prefix="/workflows",
     tags=["workflow"]
 )
 
-@agent_api.get("/")
-def test_deepauto_ai():
-    data_collector_agent()
-    # TODO: 비동기 처리
-    itinerary_builder_agent()
-    budget_manager_agent()
-    report_generator_agent()
+@agent_api.post("/")
+def test_deepauto_ai(background_task: BackgroundTasks):
+    id = start_workflow(background_task)
+    return { "status": 201, "data": { "id": id } }
---
+++ src/crud.py
@@ -0,0 +1,33 @@
+from fastapi import BackgroundTasks
+
+from src.agent import data_collector_agent, itinerary_builder_agent, budget_manager_agent, report_generator_agent
+from src.model import Workflow
+from src.status import WorkflowStatus
+from src.db import connect_database
+
+def start_workflow(background_task: BackgroundTasks):
+    """
+    agent를 차례대로 실행하는 workflow를 실행시킨다.
+    @return workflow id
+    """
+    workflow = Workflow(
+        status=WorkflowStatus.STARTED.value
+    )
+
+    with connect_database() as db:
+        # workflow 시작 상태 저장
+        db.add(workflow)
+        db.commit()
+        db.refresh(workflow)
+
+     # 2) 백그라운드에서 에이전트 실행 스케줄링
+        background_task.add_task(run_agents, workflow.id)
+
+    return workflow.id
+
+def run_agents(workflow_id: int):
+    data_collector_agent()
+    # TODO: 비동기 처리
+    itinerary_builder_agent()
+    budget_manager_agent()
+    report_generator_agent()
